39f13cb8c94d164a8192f5c6b0efe1cc1de35eab
pietrolaureano <pietrolaureano40@gmail.com>
2024-08-06 13:36:56 -0300

    Add files via upload

---
+++ ApplicationConfirmDialog/src/applicationconfirmdialog/ApplicationConfirmDialog.java
@@ -0,0 +1,16 @@
+
+package applicationconfirmdialog;
+
+import javax.swing.JOptionPane;
+
+public class ApplicationConfirmDialog {
+
+    public static void main(String[] args) {
+        JOptionPane.showConfirmDialog(null, 
+                "Deseja excluir arquivo?", 
+                "Excluir", 
+                JOptionPane.YES_NO_CANCEL_OPTION, 
+                JOptionPane.QUESTION_MESSAGE);
+    }
+    
+}
